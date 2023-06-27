b874d28aa12700ec7ad296a880fe50b7cc42961d
Nick Topacio <ntopacio25@gmail.com>
2023-06-27 19:06:07 -0400

    Add pause/resume/restart func, timer end sound; no setting yet

--- src/main.js
+++ src/main.js
@@ -3,6 +3,30 @@ import "../styles/style.css";
 import "../styles/components/timer.css";
 import "../styles/utils.css";
 
+let timerStart = retrieveTimerAmt();
+const timerEnd = 0;
+let now = timerStart;
+let progress;
+let audioVolume = 0.1;
+
+function retrieveTimerAmt() {
+  if (!window.localStorage.getItem("timer_amt")) {
+    window.localStorage.setItem("timer_amt", "1500");
+  }
+  return parseInt(window.localStorage.getItem("timer_amt"));
+}
+
+function setTimerAmt(timerAmt) {
+  window.localStorage.setItem("timer_amt", timerAmt);
+}
+
+function setInitialTimer() {
+  let timerAmt = retrieveTimerAmt();
+  let progressValue = document.querySelector(".progress-value");
+  progressValue.textContent = secondsToMinSec(timerAmt);
+  return timerAmt;
+}
+
 // converts the seconds into a readable min:sec format
 function secondsToMinSec(sec) {
   let min = Math.floor(sec / 60);
@@ -19,18 +43,15 @@ function secondsToMinSec(sec) {
   return `${min}:${secLeft}`;
 }
 
-// activates the timer
-function activateTimer(start) {
+// starts the timer
+function startTimer() {
   let circularProgress = document.querySelector(".circular-progress");
   let progressValue = document.querySelector(".progress-value");
-  progressValue.textContent = secondsToMinSec(start);
-
-  let timerStart = start;
-  let now = timerStart;
-  const timerEnd = 0;
+  let startBtn = document.querySelector(".btn-start");
+  let pauseBtn = document.querySelector(".btn-pause");
+  progressValue.textContent = secondsToMinSec(now);
   const speed = 1000; // speed in ms, 1000 ms = 1 sec
-
-  let progress = setInterval(() => {
+  progress = setInterval(() => {
     now--;
     progressValue.textContent = secondsToMinSec(now);
     circularProgress.style.background = `conic-gradient(var(--clr-dark-red) ${
@@ -38,9 +59,66 @@ function activateTimer(start) {
     }deg, var(--clr-lightest-red) 0deg)`;
 
     if (now == timerEnd) {
+      progressValue.textContent = "0:00";
+      let completedAudio = new Audio("../audio/success.mp3");
+      startBtn.disabled = true;
+      pauseBtn.disabled = true;
+      completedAudio.volume = audioVolume;
+      completedAudio.play();
       clearInterval(progress);
     }
   }, speed);
 }
 
-activateTimer(10);
+function listenForStartBtn() {
+  let startBtn = document.querySelector(".btn-start");
+  let pauseBtn = document.querySelector(".btn-pause");
+  startBtn.addEventListener("click", () => {
+    // console.log("start!");
+    startBtn.style.display = "none";
+    pauseBtn.style.display = "block";
+    startTimer();
+  });
+}
+
+function listenForPauseBtn() {
+  let startBtn = document.querySelector(".btn-start");
+  let pauseBtn = document.querySelector(".btn-pause");
+  pauseBtn.addEventListener("click", () => {
+    // console.log("pause!");
+    startBtn.style.display = "block";
+    pauseBtn.style.display = "none";
+    clearInterval(progress);
+  });
+}
+
+function listenForRestartBtn() {
+  let startBtn = document.querySelector(".btn-start");
+  let pauseBtn = document.querySelector(".btn-pause");
+  let restartBtn = document.querySelector(".btn-restart");
+  let circularProgress = document.querySelector(".circular-progress");
+  let progressValue = document.querySelector(".progress-value");
+  restartBtn.addEventListener("click", () => {
+    // console.log("restart!");
+    now = timerStart;
+    progressValue.textContent = secondsToMinSec(now);
+    circularProgress.style.background =
+      "conic-gradient(var(--clr-dark-red) 360deg, var(--clr-lightest-red) 0deg)";
+    startBtn.style.display = "block";
+    pauseBtn.style.display = "none";
+    startBtn.disabled = false;
+    pauseBtn.disabled = false;
+    clearInterval(progress);
+  });
+}
+
+function listenForSettingsBtn() {}
+
+function listenToAllBtns() {
+  listenForStartBtn();
+  listenForPauseBtn();
+  listenForRestartBtn();
+}
+
+setInitialTimer();
+listenToAllBtns();
